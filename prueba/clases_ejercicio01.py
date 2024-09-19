56b2dc5dc0214b786d119e61ceae5b488ca661da
Keily-jh <202411882@urp.edu.pe>
2024-09-19 17:22:05 +0000

    corregido

--- prueba/clases_ejercicio01.py
+++ prueba/clases_ejercicio01.py
@@ -4,20 +4,23 @@ Fecha: 17/09/2024
 '''
 class Empleado:
     def __init__(self, nombre, edad, salario):
-        self.nombre =nombre
+        self.nombre = nombre
         self.edad = edad
         self.salario = salario
-    def calcular_bono (self):
-        return self.salario * 0.1
-    def __str__(self):
-        return f'Empleado => Nombre: {self.nombre}, Edad: {self.edad}, Salario: {self.salario}'
 
+    def calcular_bono(self):
+        return self.salario * 0.1
+    
+    def __str__(self):
+        return f'Empleado=> nombre: {self.nombre}, edad: {self.edad}, salario: {self.salario}'
+    
 class Gerente(Empleado):
     def __init__(self, nombre, edad, salario, departamento):
         super().__init__(nombre, edad, salario)
         self.departamento = departamento
+    
     def calcular_bono(self):
         return self.salario * 0.2
     
     def __str__(self):
-        return super().__str__() + f', departamento: {self.departamento}'
+        return super().__str__() + f', departamento: {self.departamento}' 
